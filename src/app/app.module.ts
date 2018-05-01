3ab74c9c13ad37483bf3919272266891cb58c2f7
junweipan <panjunweide@gmail.com>
2018-05-01 12:41:42 +1000

    complete admin functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { ReservationsComponent } from './admin/reservations/reservations.component';
+import { ReservationComponent } from './admin/reservations/reservation/reservation.component';
+import { ReservationListComponent } from './admin/reservations/reservation-list/reservation-list.component';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+
+import {ToastrModule} from 'ngx-toastr';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ReservationsComponent,
+    ReservationComponent,
+    ReservationListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConifg),
+    AngularFireDatabaseModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BsDatepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
